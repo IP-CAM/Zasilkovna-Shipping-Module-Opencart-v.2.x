a68bdbb7b59cdcc85cabf40cb075d2500dcd1608
rivfaader <glucfiilp@gmail.com>
2015-06-12 15:08:24 +0200

    Added HU and PL option for branch list

--- admin/controller/shipping/zasilkovna.php
+++ admin/controller/shipping/zasilkovna.php
@@ -1,6 +1,6 @@
 <?php
 class ControllerShippingZasilkovna extends Controller {
-	public 	$countries 		= array('cz'=>'Česká republika','sk'=>'Slovenská republika',''=>'vše');
+	public 	$countries 		= array('cz'=>'Česká republika', 'hu'=>'Maďarsko', 'pl'=>'Polsko', 'sk'=>'Slovenská republika', ''=>'vše');
 	public  $_servicesCnt 	= 6;	
 	public 	$inputFields 	= array('price'=>'price','js'=>'js','title'=>'title','destination'=>'destination','freeover'=>'freeover');
 	private $error 			= array(); 
